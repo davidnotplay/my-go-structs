d79c9ba4c56eae826be98b7e3deb3ac5ebcf8bd1
davidnotplay <dcasadomartinez@gmail.com>
2019-07-31 16:05:03 +0200

    Make first approach to the AVL

---
+++ list/list.go
@@ -0,0 +1,96 @@
+package list
+
+import (
+	"fmt"
+)
+
+// max returns the param more large
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+// treeNode is the interanal AVL tree node
+type treeNode struct {
+	ltree, rtree *treeNode
+	height       int
+	value        ListValue
+}
+
+// Height returns the `node.height` property. If node is nil, then returns -1
+func (node *treeNode) Height() int {
+	if node != nil {
+		return node.height
+	}
+
+	return -1
+}
+
+// maxHeights returns the max value of left tree height and right tree height
+func (node treeNode) maxHeight() int {
+	return max(node.ltree.Height(), node.rtree.Height())
+}
+
+// rotateRight execute an AVL tree right rotation.
+func (node *treeNode) rotateRight() *treeNode {
+	newNode := node.ltree
+	node.ltree = newNode.rtree
+	newNode.rtree = node
+
+	newNode.height = newNode.maxHeight() + 1
+	node.height = node.maxHeight() + 1
+
+	return newNode
+}
+
+// rotateLeft execute an AVL tree left rotation.
+func (node *treeNode) rotateLeft() *treeNode {
+	newNode := node.rtree
+	node.rtree = newNode.ltree
+	newNode.ltree = node
+
+	newNode.height = newNode.maxHeight() + 1
+	node.height = node.maxHeight() + 1
+
+	return newNode
+}
+
+// rotateRightLeft executes the AVL double rotation (right left)
+func (node *treeNode) rotateRightLeft() *treeNode {
+	node.rtree = node.rtree.rotateRight()
+	return node.rotateLeft()
+}
+
+// rotateLeftRight executes the AVL double rotation (left right)
+func (node *treeNode) rotateLeftRight() *treeNode {
+	node.ltree = node.ltree.rotateLeft()
+	return node.rotateRight()
+}
+
+func (node *treeNode) stringifyNode(sep string, space string) string {
+	if node == nil {
+		return "NULL\n"
+	}
+
+	valueStr :=  node.value.String()
+	lChildStr := node.ltree.stringifyNode(fmt.Sprintf("%s│%s", sep, space), space)
+	rChildStr := node.rtree.stringifyNode(fmt.Sprintf("%s%s%s", sep, space, space), space)
+	return fmt.Sprintf("%s\n%s├─%s%s└─%s", valueStr, sep, lChildStr, sep, rChildStr)
+}
+
+func (node treeNode) Stringify() string {
+	return node.StringifyWithIndent(" ")
+}
+
+func (node treeNode) StringifyWithIndent(indent string) string {
+	return node.stringifyNode("", indent)
+}
+
+type ListValue interface {
+	Less(ListValue) bool
+	Eq(ListValue) bool
+	String() string
+}
---
+++ list/list_test.go
@@ -0,0 +1,173 @@
+package list
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"fmt"
+)
+
+type intValue struct {
+	value int
+}
+
+func (i intValue) String() string {
+	return fmt.Sprintf("%d", i.value)
+}
+
+func (i intValue) Less(v ListValue) bool {
+	vi, _ := v.(*intValue)
+	return i.value < vi.value
+}
+
+func (i intValue) Eq(v ListValue) bool {
+	vi, _ := v.(*intValue)
+	return i.value == vi.value
+}
+
+func i(i int) *int {
+	return &i
+}
+
+func checkTree(t *testing.T, node *treeNode, l, r *int) {
+
+	if l == nil {
+		assert.Nil(t, node.ltree)
+	} else {
+		v, _ := node.ltree.value.(*intValue)
+		assert.Equal(t, v.value, *l)
+	}
+
+	if r == nil {
+		assert.Nil(t, node.rtree)
+	} else {
+		v, _ := node.rtree.value.(*intValue)
+		assert.Equal(t, v.value, *r)
+	}
+
+}
+
+// Tests
+func Test_max_func(t *testing.T) {
+	assert.Equal(t, max(2, 3), 3)
+	assert.Equal(t, max(4, 1), 4)
+	assert.Equal(t, max(2, 2), 2)
+
+	assert.Equal(t, max(-2, -3), -2)
+	assert.Equal(t, max(-4, -1), -1)
+	assert.Equal(t, max(-2, -2), -2)
+
+	assert.Equal(t, max(2, -2), 2)
+	assert.Equal(t, max(1, -2), 1)
+	assert.Equal(t, max(-1, 2), 2)
+}
+
+func Test_treeNode_Height_func(t *testing.T) {
+	as := assert.New(t)
+
+	as.Equal((*treeNode)(nil).Height(), -1, "When node is nil, must returns -1")
+	as.Equal((&treeNode{nil, nil, 33, nil}).Height(), 33, "Return invalid height")
+}
+
+func Test_treeNode_maxHeight_func(t *testing.T) {
+	as := assert.New(t)
+
+	node := treeNode{nil, nil, 3, nil}
+	as.Equal(node.maxHeight(), -1, "children are nil, so the height of the node must be -1")
+
+	node = treeNode{&treeNode{nil, nil, 22, nil}, nil, 3, nil}
+	as.Equal(node.maxHeight(), 22, "height value is the height value of the left child")
+
+	node = treeNode{nil, &treeNode{nil, nil, 41, nil}, 3, nil}
+	as.Equal(node.maxHeight(), 41, "height value is the height value of the left child")
+
+	node = treeNode{&treeNode{nil, nil, 15, nil}, &treeNode{nil, nil, 30, nil}, 3, nil}
+	as.Equal(node.maxHeight(), 30, "height must be 30")
+}
+
+func Test_treeNode_rotateRight_func(t *testing.T) {
+	tree1 := &treeNode{nil, nil, -1, &intValue{1}}
+	tree2 := &treeNode{nil, nil, -1, &intValue{2}}
+	tree3 := &treeNode{nil, nil, -1, &intValue{3}}
+	tree4 := &treeNode{nil, nil, -1, &intValue{4}}
+	tree5 := &treeNode{nil, nil, -1, &intValue{5}}
+
+	tree2.ltree = tree1
+	tree2.rtree = tree3
+	tree4.ltree = tree2
+	tree4.rtree = tree5
+
+	ntree := tree4.rotateRight()
+
+	checkTree(t, ntree, i(1), i(4))
+	checkTree(t, tree1, nil, nil)
+	checkTree(t, tree2, i(1), i(4))
+	checkTree(t, tree3, nil, nil)
+	checkTree(t, tree4, i(3), i(5))
+	checkTree(t, tree5, nil, nil)
+}
+
+func Test_treeNode_rotateLeft_func(t *testing.T) {
+	tree1 := &treeNode{nil, nil, -1, &intValue{1}}
+	tree2 := &treeNode{nil, nil, -1, &intValue{2}}
+	tree3 := &treeNode{nil, nil, -1, &intValue{3}}
+	tree4 := &treeNode{nil, nil, -1, &intValue{4}}
+	tree5 := &treeNode{nil, nil, -1, &intValue{5}}
+
+	tree4.ltree = tree3
+	tree4.rtree = tree5
+	tree2.ltree = tree1
+	tree2.rtree = tree4
+
+	ntree := tree2.rotateLeft();
+
+	checkTree(t, ntree, i(2), i(5))
+	checkTree(t, tree1, nil, nil)
+	checkTree(t, tree2, i(1), i(3))
+	checkTree(t, tree3, nil, nil)
+	checkTree(t, tree4, i(2), i(5))
+	checkTree(t, tree5, nil, nil)
+}
+
+func Test_treeNode_rotateRightLeft_func(t *testing.T) {
+	tree1 := &treeNode{nil, nil, -1, &intValue{1}}
+	tree2 := &treeNode{nil, nil, -1, &intValue{2}}
+	tree3 := &treeNode{nil, nil, -1, &intValue{3}}
+	tree4 := &treeNode{nil, nil, -1, &intValue{4}}
+	tree5 := &treeNode{nil, nil, -1, &intValue{5}}
+
+	tree4.ltree = tree3
+	tree4.rtree = tree5
+	tree2.ltree = tree1
+	tree2.rtree = tree4
+
+	ntree := tree2.rotateRightLeft()
+
+	checkTree(t, ntree, i(2), i(4))
+	checkTree(t, tree1, nil, nil)
+	checkTree(t, tree2, i(1), nil)
+	checkTree(t, tree3, i(2), i(4))
+	checkTree(t, tree4, nil, i(5))
+	checkTree(t, tree5, nil, nil)
+}
+
+func Test_treeNode_rotateLeftRight_func(t *testing.T) {
+	tree1 := &treeNode{nil, nil, -1, &intValue{1}}
+	tree2 := &treeNode{nil, nil, -1, &intValue{2}}
+	tree3 := &treeNode{nil, nil, -1, &intValue{3}}
+	tree4 := &treeNode{nil, nil, -1, &intValue{4}}
+	tree5 := &treeNode{nil, nil, -1, &intValue{5}}
+
+	tree2.ltree = tree1
+	tree2.rtree = tree3
+	tree4.ltree = tree2
+	tree4.rtree = tree5
+
+	ntree := tree4.rotateLeftRight()
+
+	checkTree(t, ntree, i(2), i(4))
+	checkTree(t, tree1, nil, nil)
+	checkTree(t, tree2, i(1), nil)
+	checkTree(t, tree3, i(2), i(4))
+	checkTree(t, tree4, nil, i(5))
+	checkTree(t, tree1, nil, nil)
+}
