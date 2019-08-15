e6d9163f136fae8e90a8468d453fbbbfc6345448
davidnotplay <dcasadomartinez@gmail.com>
2019-08-15 20:39:51 +0200

    Make benchmarks

---
+++ benchmark_test.go
@@ -0,0 +1,54 @@
+package structs
+
+import "testing"
+
+
+func insertValues(num int) Avl {
+	avl := NewAvl()
+	var v Value
+
+	for i := 0; i < num; i++ {
+		v = intValue{i}
+		avl.Insert(&v)
+	}
+
+	return avl
+}
+
+func insertValuesBenchmark(num int, b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		println(i)
+		insertValues(num)
+	}
+}
+
+func Benchmark_AVLInsert1(b *testing.B) {
+	insertValuesBenchmark(1, b)
+}
+
+func Benchmark_AVLInsert10(b *testing.B) {
+	insertValuesBenchmark(10, b)
+}
+
+func Benchmark_AVLInsert100(b *testing.B) {
+	insertValuesBenchmark(100, b)
+}
+
+func Benchmark_AVLInsert1000(b *testing.B) {
+	insertValuesBenchmark(1000, b)
+}
+func Benchmark_AVLInsert10000(b *testing.B) {
+	insertValuesBenchmark(10000, b)
+}
+
+func Benchmark_AVLInsert100000(b *testing.B) {
+	insertValuesBenchmark(100000, b)
+}
+
+func Benchmark_AVLInsert1000000(b *testing.B) {
+	insertValuesBenchmark(1000000, b)
+}
+
+func Benchmark_AVLInsert10000000(b *testing.B) {
+	insertValuesBenchmark(1000000, b)
+}
