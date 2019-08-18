1366d9ef2b7d65e89d859c4aac5e62a815d70c9f
davidnotplay <dcasadomartinez@gmail.com>
2019-08-19 01:04:37 +0200

    Create the internal list struct
    Created an internal struct called *internal_list*. It will be used for
    create other structs as: queue, stack lists, etc. This new struct
    contains the methods necessary and will be the base for the new lineal
    structs.

---
+++ internal_list_test.go
@@ -0,0 +1,6 @@
+package structs
+
+/*
+ All methods of the internalList are used and are very similiar to the methods
+ of the List struct. So the method tests are wrote in the list_test.go file.
+*/
